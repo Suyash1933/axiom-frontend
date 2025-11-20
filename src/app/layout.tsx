88c87233f4980b3ea796ed7a3a1f5015ce8b960e
Suyash1933 <122913472+Suyash1933@users.noreply.github.com>
2025-11-20 08:18:47 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,33 @@
+// src/app/layout.tsx
+import React from "react";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import SubNavbar from "./components/SubNavbar";
-import Footer from "./components/Footer";
 import TriplePairs from "./components/TriplePairs";
+import Footer from "./components/Footer";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
-        {/* ⭐ Font Awesome CDN */}
+        {/* Font Awesome CDN (you already used this) */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
       </head>
 
-      <body className="bg-black">
+      {/* add bottom padding so fixed footer doesn't overlap content */}
+      <body className="bg-black pb-16 text-white">
         <Navbar />
         <SubNavbar />
         <TriplePairs />
         {children}
-         <Footer />
+        <Footer />
       </body>
     </html>
   );
